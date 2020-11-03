1dcd1aee8136f052ba81b85d98c1eaa7c6caeb93
keyih <keyih@sfu.ca>
2020-11-02 21:20:21 -0800

    setup home page

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static("./client/img"))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
+app.get('/bg_img.jpg', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../img/bg_img.jpg'));
+});
 
 app.listen(8080, () => {
   console.log('App is running...');
