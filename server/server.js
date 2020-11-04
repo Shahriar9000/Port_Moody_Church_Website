c632db43e53ece2af02986e98ef5a9fa2a5554b1
keyih <keyih@sfu.ca>
2020-11-04 00:15:43 -0800

    add db connection

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static("./client/img"))
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/index.html'));
@@ -12,6 +12,6 @@ app.get('/bg_img.jpg', (req, res) => {
   res.sendFile(path.join(__dirname + '/../img/bg_img.jpg'));
 });
 
-app.listen(8080, () => {
-  console.log('App is running...');
+app.listen( process.env.PORT || '8080', () => {
+  console.log(`Server is running on port: ${process.env.POST || '8080'}`);
 });
