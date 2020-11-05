9255737ad0c068b4f0a0c0f43de8b47fad54cdf9
USER <USER@sfu.ca>
2020-11-05 02:31:09 -0800

    added basic content for donation page. donation button via paypal donation service for now.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static("./client/img"))
+/*app.use(express.static("./client/img"))*/
+app.use(express.static(path.join(__dirname, '/../client')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/index.html'));
@@ -10,6 +11,10 @@ app.get('/', (req, res) => {
 
 app.get('/bg_img.jpg', (req, res) => {
   res.sendFile(path.join(__dirname + '/../img/bg_img.jpg'));
+});
+
+app.get('/donation', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/donation.html'));
 });
 
 app.listen(8080, () => {
