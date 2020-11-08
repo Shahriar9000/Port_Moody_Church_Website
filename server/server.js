6d64484499b37cf50989f992a098e95461cf981c
keyih <keyih@sfu.ca>
2020-11-08 02:08:16 -0800

    bugs fixed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const db = require('./db/index');
 
