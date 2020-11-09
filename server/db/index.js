76dced304520dc765f0cf8fbcb366eca7372cece
Ngawang (Choenden) Kyirong <ckyirong@sfu.ca>
2020-11-08 16:52:33 -0800

    added google resource for SQL and .env file

--- server/db/index.js
+++ server/db/index.js
@@ -4,12 +4,21 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: path.join(__dirname + '/../../.env')});
 
+/*
 const db = mysql.createConnection({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: 'cmpt470',
   host: 'localhost',
 })
+*/
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS
+});
 
 db.connect(function(error) {
   if (!!error) {
