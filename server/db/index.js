22d7f374ae2f07cfa07d3cb88b349b4655a23781
Ngawang (Choenden) Kyirong <ckyirong@sfu.ca>
2020-12-01 15:14:15 -0800

    added new db connection

--- server/db/index.js
+++ server/db/index.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: 'cmpt470',
-  host: 'localhost',
+  host: '10.138.0.2',
 })
 
 db.connect(function(error) {
