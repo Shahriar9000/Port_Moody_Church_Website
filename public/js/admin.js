a56cdcab804bc21b29525d5d4d89ead751382b65
wuhuiw <wuhuiw@sfu.ca>
2020-11-30 21:38:13 -0800

    added route for admin features

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,46 @@
 
+showAllUsers();
 
-window.onload = function() {
-    var tbody = document.getElementById('tbMain');
+function showAllUsers() {
+    var request = new XMLHttpRequest();
+    var requestURL = '/admin/getTable'
+    request.open('GET', requestURL);
+    request.send();
+    request.onload = function() {
+        var users = request.response;
+        printUserTable(users);
+    }
+}
+
+function printUserTable(users) {
+    var table = document.getElementById('tbMain');
+
+    for (var i in users) {
+        const name = users[i].name;
+        const email = users[i].email;
+
+        var user_card = document.createElement("div");
+        user_card.classList.add("mb-2");
+
+        var user_table = document.createElement("table");
+        var user_row = user_table.insertRow(i);
+        var user_cell = user_row.insertCell(i);
+        
+        user_cell = user_row.insertCell(i+1);
+        user_table.classList.add("");
+        user_table.id = "user_table_" + i;
+        user_cell.innerHTML = name;
+        user_cell = user_row.insertCell(i+1);
+        user_cell.innerHTML = email;
+
+        var c, r, t;
+    t = document.createElement('table');
+    r = t.insertRow(0); 
+    c = r.insertCell(0);
+    c.innerHTML = 123;
+    c = r.insertCell(1);
+    c.innerHTML = 456;
+    document.getElementById("addtable").appendChild(t);
+
+    }
 }
--- server/route/admin.js
+++ server/route/admin.js
@@ -28,4 +28,34 @@ router.get('/', (req, res, next) => {
   res.render('admin.ejs', {userId, role});
 }); 
 
+router.get('/admin.js', (req, res) => {
+    res.sendFile(path.join(__dirname + '../../../public/js/admin.js'));
+});
+
+router.get('/getTable', (req, res) => {
+	const user_id = req.params.id;
+	const query = "SELECT * FROM users WHERE role = 'regular' AND id =" + user_id;
+	db.query(query, (err, rows, fields) => {
+		if(err) {
+			console.Consolelog("Failed to query: " + err);
+		}
+		console.log("Getting user data from database");
+		res.json(rows);
+	})
+});
+
+router.post('/delete_user/:id', (req, res) => {
+	const user_id = req.params.id;
+	const query = "DELETE FROM users WHERE id = ?";
+	db.query(query, [user_id], (err, rows, fields) => {
+		if(err) {
+			console.log("Failed to delete users " + user_id + " " + err);
+		} else{
+			console.log("Deleted user: " + user_id);
+		}
+		res.redirect('/admin');
+	})
+});
+
+
 module.exports = router;
