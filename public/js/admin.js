5deca0594d76464bc87c39e1941fca4bc25ae9af
wuhuiw <wuhuiw@sfu.ca>
2020-12-01 23:26:13 -0800

    added style for user table on admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -21,24 +21,21 @@ function printUserTable(users) {
         const name = users[i].name;
         const email = users[i].email;
 
-        // var user_card = document.createElement("div");
-        // user_card.classList.add("mb-2");
-
         var user_table = document.createElement("tbody");
         var user_row = user_table.insertRow();
         var user_cell1 = user_row.insertCell(0);
         var user_cell2 = user_row.insertCell(1);
         var user_cell3 = user_row.insertCell(2);
         
-        // user_table.classList.add("");
         user_cell1.innerHTML = name;
         user_cell2.innerHTML = email;
 
         var delete_button = document.createElement("form");
         delete_button.setAttribute("method", "post");
-        delete_button.classList.add("btn", "btn-secondary", "mt-2");
+        delete_button.classList.add("btn", "btn-outline-secondary", "mt-2");
         delete_button.id = "delete_user_" + user_id;
         delete_button.setAttribute("onclick", "deleteUser(" + user_id + ")");
+        
         delete_button.innerHTML = "Delete";
 
         user_cell3.appendChild(delete_button);
