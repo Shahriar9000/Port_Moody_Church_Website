78860a91fc30d10747fe0fc2c9103d16ba356fb7
marefin <marefin@sfu.ca>
2020-12-04 14:53:46 -0800

    changed db/index config to original

--- server/db/index.js
+++ server/db/index.js
@@ -4,18 +4,18 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: path.join(__dirname + '/../../.env')});
 
-// const db = mysql.createConnection({
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: 'cmpt470',
-//   host: 'localhost',
-// })
 const db = mysql.createConnection({
-  host: "db4free.net",
-  user: 'shahriar',
-  password: '1q2w3e4r5t6y',
-  database: 'database_cmpt'
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: 'cmpt470',
+  host: 'localhost',
 })
+// const db = mysql.createConnection({
+//   host: "db4free.net",
+//   user: 'shahriar',
+//   password: '1q2w3e4r5t6y',
+//   database: 'database_cmpt'
+// })
 
 db.connect(function(error) {
   if (!!error) {
